bdcac3e0430d4f4af5658d366e21e5da031d8ee7
Wojtek <42886283+WojtekFront@users.noreply.github.com>
2021-01-24 12:27:59 +0100

    script edit
    add exceptions
    add comments
    subtract old functions

--- js/script.js
+++ js/script.js
@@ -1,43 +1,46 @@
 function GetSelectedValue() {
-
-    let textArea, newTextArea, ilosc, wydruk, returnValue, countValue;
+    //creating variable
+    //const textArea, ilosc;
+    let ilosc, textArea, returnValue, countValue;
 
     textArea = document.getElementById("textArea").value;
     ilosc = document.getElementById("ilosc").value;
-    wydruk = document.getElementById("wydruk").innerHTML;
-    //const number = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'];
+    countValue = 0;
 
-
+    //logic in select
     if (textArea.length > 0) {
         returnValue = "";
-        countValue = 0;
         if (ilosc == "iloscZnakow") {
             returnValue = textArea.split("");
-            countValue = returnValue.length;
         }
         else if (ilosc == "iloscCyfr") {
-            returnValue = textArea.match(/\d/g);
-            countValue = returnValue.length;
+            returnValue = textArea.match(/\d/g); //d is digit
         }
         else if (ilosc == "iloscLiter") {
-            returnValue = textArea.match(/[A-Za-ząćęłńóśźżĄĆĘŁŃÓŚŹŻ]/gi);
-            countValue = returnValue.length;
+            returnValue = textArea.match(/[a-ząćęłńóśźż]/gi); //i-case-insensitive search  A-ZĄĆĘŁŃÓŚŹŻ
         }
         else if (ilosc == "iloscWyrazow") {
-            String.prototype.countWords = function () {            
-                return this.split(/\s+\b/);
+            String.prototype.countWords = function () {
+                return this.split(/\s+\b/g); //split by whitespace and begining with letter
             }
-            newTextArea = textArea.trim();
-            returnValue = newTextArea.countWords();
+            textArea = textArea.trim();  //without trim plus one word on begining
+            returnValue = textArea.countWords();
+        }
+
+        if (returnValue == "" || returnValue == null) { //catching exceptions
+            returnValue = "zbiór jest pusty";
+            countValue = 0;
+        } else {
             countValue = returnValue.length;
         }
+
+
     } else {
-        returnValue = "zbiór jest pusty.";
-        countValue = 0;
+        returnValue = "zbiór jest pusty";
     }
+
+    //print in: console.log and screen
     console.log(returnValue);
     document.getElementById("wydruk").innerText = countValue;
-
-
 
 }
