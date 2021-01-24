9ca81445fa2c812b54ab2994d4495a117d276027
Wojtek <42886283+WojtekFront@users.noreply.github.com>
2021-01-24 11:26:06 +0100

    repaire select JS
    all functions in select(ilosc wyrazow, dlugosc tekstu, ilosc cyfr, ilosc liter) working fine

--- js/script.js
+++ js/script.js
@@ -5,58 +5,38 @@ function GetSelectedValue() {
     textArea = document.getElementById("textArea").value;
     ilosc = document.getElementById("ilosc").value;
     wydruk = document.getElementById("wydruk").innerHTML;
-    const number = ['1','2','3','4','5','6','7','8','9','0'];
-    const letters = ['a','b','c',]
+    //const number = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'];
 
-if(textArea.length>0)
-{ 
-returnValue="";
-countValue=0;
-   // console.log(textArea.length);
-   if (ilosc == "iloscZnakow") {
-        returnValue = textArea.length;
-        countValue = returnValue;
-    }
-    else if (ilosc == "iloscCyfr") {
 
-        // for (let value of textArea) {
-        //     if (number.includes(value)){
-        //         returnValue +=value;
-        //         countValue++;
-
-        //     }
-        // }
-        //alternative options for counting
-        countValue= textArea.match(/\d/g).length;
-       
-
-      
-    }
-    else if (ilosc == "iloscLiter") {
-
-        for (let value of textArea) {
-            if (isNaN(value)){
-                returnValue +=value;
-                countValue++;
-            }
+    if (textArea.length > 0) {
+        returnValue = "";
+        countValue = 0;
+        if (ilosc == "iloscZnakow") {
+            returnValue = textArea.split("");
+            countValue = returnValue.length;
         }
+        else if (ilosc == "iloscCyfr") {
+            returnValue = textArea.match(/\d/g);
+            countValue = returnValue.length;
+        }
+        else if (ilosc == "iloscLiter") {
+            returnValue = textArea.match(/[A-Za-ząćęłńóśźżĄĆĘŁŃÓŚŹŻ]/gi);
+            countValue = returnValue.length;
+        }
+        else if (ilosc == "iloscWyrazow") {
+            String.prototype.countWords = function () {            
+                return this.split(/\s+\b/);
+            }
+            newTextArea = textArea.trim();
+            returnValue = newTextArea.countWords();
+            countValue = returnValue.length;
+        }
+    } else {
+        returnValue = "zbiór jest pusty.";
+        countValue = 0;
     }
-    else if (ilosc == "iloscWyrazow") {
-        String.prototype.countWords = function(){
-            return this.split(/\s+\b/).length;
-          }
-          
-          countValue = textArea.countWords();
-
-
-
-    }
-}else{
-    returnValue="zbiór jest pusty";
-    countValue=0;
-}
- console.log(returnValue);
- document.getElementById("wydruk").innerText=countValue;
+    console.log(returnValue);
+    document.getElementById("wydruk").innerText = countValue;
 
 
 
